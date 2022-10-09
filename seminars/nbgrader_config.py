a75aababaa8fc41b30660b8fc66b8346ae28ec53
rprofeta <30694071+rprofeta@users.noreply.github.com>
2022-10-09 18:16:04 +0200

    Update nbgrader_config.py

--- seminars/nbgrader_config.py
+++ seminars/nbgrader_config.py
@@ -1,6 +1,8 @@
 #According to: https://nbgrader.readthedocs.io/en/stable/user_guide/managing_assignment_files.html#setting-up-the-exchange
 c = get_config()
-c.CourseDirectory.course_id = "adsp_ws2022"
+c.CourseDirectory.course_id = "CA_seminars_ws22"
 c.Exchange.root = "/home/fera/WS22_Seminars/instructor/nextcloud"
 c.CourseDirectory.student_id = "jimihendrix"
+
+
 
